4a0ece45981e4e35b6076fcfbfc56b67fab8df7e
Siddhant Kohli <kohli.si@northeastern.edu>
2022-11-23 01:18:27 -0500

    interaction updated

--- psafinal/src/main/java/psafinal/interact.java
+++ psafinal/src/main/java/psafinal/interact.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
@@ -26,10 +28,22 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class interact {
 	  public static final int WORD_LENGTH = 5;
-	    public static final int ATTEMPTS = 5;
-	    public static final int SUGGESTION_COUNT = 10;
+	    public static final int ATTEMPTS = 6;
+	    //public static final int SUGGESTION_COUNT = 10;
+	    
 	public static void main(String[] args ) throws IOException, InterruptedException {
+		File file = new File("wordlist/valid-wordle-words.txt");
+		BufferedReader br= new BufferedReader(new FileReader(file));
+		String st;
+		HashSet<String> wordSet = new HashSet<String>();
+
+		while ((st = br.readLine()) != null )
+		{
+			wordSet.add(st);
+		}
 		
+		
+				
 		
 		WebDriver driver =WebDriverManager.chromedriver().create();
 	      driver.get("https://www.nytimes.com/games/wordle/index.html");
@@ -40,26 +54,42 @@ public class interact {
 	      web.click();
 	      driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	      Actions act=new Actions(driver);
-	      String word="";
+	      String word="caste";
 	      
-	      for(int outerdiv=1;outerdiv<=2;outerdiv++) {
-	      for(int innerdiv=1;innerdiv<=5;innerdiv++) {
+	      for(int outerdiv=1;outerdiv<=1;outerdiv++) {
+	    	  
+	      for(int innerdiv=1;innerdiv<=WORD_LENGTH;innerdiv++) {
 	    	  web=driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div/div["+outerdiv+"]/div["+innerdiv+"]/div"));
 	    	  Thread.sleep(2000);
 	    	  act.sendKeys(word.charAt(innerdiv-1)+"").perform();
-	      System.out.println(web.getLocation()+" | ");
+	      
 	      }
 	      act.sendKeys(Keys.ENTER).perform();
 	      Thread.sleep(5000);
-	      for(int innerdiv=1;innerdiv<=5;innerdiv++) {
+	      
+	      
+	      for(int innerdiv=1;innerdiv<=WORD_LENGTH;innerdiv++) {
 	    	  web=driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div/div["+outerdiv+"]/div["+innerdiv+"]/div"));
-	    	  System.out.print(web.getAttribute("outerHTML")+" ");
-	    	 
-	    	  //web.sendKeys("c");
-	      System.out.println(web.getLocation()+" | ");
+	    	  String txt=web.getAttribute("outerHTML".valueOf("data-state")).split(" ").clone()[0];
+	    	  System.out.println(txt);
+	    	  if(txt.equals("correct")) {
+	    		  System.out.println(word.charAt(innerdiv-1));
+	    		  removeDictionarywords(wordSet,word.charAt(innerdiv-1),innerdiv-1,txt);
+	    	  }
+	    	  else if(txt.equals("present")) {
+	    		  removeDictionarywords(wordSet,word.charAt(innerdiv-1),innerdiv-1,txt);
+	    	  }
+	    	  else if(txt.equals("absent")){
+	    		  //absent
+	    		  removeDictionarywords(wordSet,word.charAt(innerdiv-1),innerdiv-1,txt);
+	    		  //System.out.println(word.charAt(innerdiv-1));
+	    	  }
+	    
 	      }
-	     
-	      System.out.println("------------------");
+	      
+	      driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
+	     // System.out.println("------------------");
+	      
 	      }
 
 
@@ -67,6 +97,31 @@ public class interact {
 	        System.out.println("url" + driver.getCurrentUrl());
 	        //driver.quit();
 	        
+	}
+
+	private static void removeDictionarywords(HashSet<String> wordSet, char chartocheck, int charposition, String method) {
+		// TODO Auto-generated method stub
+		System.out.println("wordset size before  ---- " +wordSet.size());
+		Iterator<String> iterator = wordSet.iterator();
+		while (iterator.hasNext()) {
+		    String element = iterator.next();
+		    if(method.equals("correct")) {
+		    	if(element.charAt(charposition)!=chartocheck) {
+		    	iterator.remove();
+		    	}
+		    }
+		    else if(method.equals("present") || method.equals("absent")){
+		    	if(element.charAt(charposition)==chartocheck) {
+					iterator.remove();
+				    }	
+		    }
+		    
+		}
+		for(String s:wordSet) {
+			System.out.println(s);
+		}
+		 System.out.println("wordset size after  ---- " +wordSet.size());
+		
 	}
 }
 
