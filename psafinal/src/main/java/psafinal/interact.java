7f1f906bfa9e5a4443770e5d0b1e44baef1a3221
Siddhant Kohli <kohli.si@northeastern.edu>
2022-11-23 03:00:56 -0500

    interaction updated

--- psafinal/src/main/java/psafinal/interact.java
+++ psafinal/src/main/java/psafinal/interact.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -22,6 +23,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.Select;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -42,21 +44,26 @@ public class interact {
 			wordSet.add(st);
 		}
 		
+		ChromeOptions co=new ChromeOptions();
+		co.addArguments("−−incognito");
 		
-				
+		WebDriver driver =WebDriverManager.chromedriver().capabilities(co).create();
 		
-		WebDriver driver =WebDriverManager.chromedriver().create();
 	      driver.get("https://www.nytimes.com/games/wordle/index.html");
+	      
+	      //waiting for popup to show
 	      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	      
 	      // to close the pop-up page
 	      WebElement web=driver.findElement(By.className("Modal-module_closeIcon__b4z74"));
 	      web.click();
+	      
+	      
 	      driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	      Actions act=new Actions(driver);
-	      String word="caste";
+	      String word="prize";
 	      
-	      for(int outerdiv=1;outerdiv<=1;outerdiv++) {
+	      for(int outerdiv=1;outerdiv<=2;outerdiv++) {
 	    	  
 	      for(int innerdiv=1;innerdiv<=WORD_LENGTH;innerdiv++) {
 	    	  web=driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div/div["+outerdiv+"]/div["+innerdiv+"]/div"));
@@ -72,26 +79,61 @@ public class interact {
 	    	  web=driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div/div["+outerdiv+"]/div["+innerdiv+"]/div"));
 	    	  String txt=web.getAttribute("outerHTML".valueOf("data-state")).split(" ").clone()[0];
 	    	  System.out.println(txt);
-	    	  if(txt.equals("correct")) {
-	    		  System.out.println(word.charAt(innerdiv-1));
-	    		  removeDictionarywords(wordSet,word.charAt(innerdiv-1),innerdiv-1,txt);
-	    	  }
-	    	  else if(txt.equals("present")) {
+	    	  if(txt.equals("correct")|| txt.equals("present")) {
+	    		  
 	    		  removeDictionarywords(wordSet,word.charAt(innerdiv-1),innerdiv-1,txt);
 	    	  }
 	    	  else if(txt.equals("absent")){
 	    		  //absent
-	    		  removeDictionarywords(wordSet,word.charAt(innerdiv-1),innerdiv-1,txt);
-	    		  //System.out.println(word.charAt(innerdiv-1));
+	    		  //int common=0;
+	    		  HashSet<String> checkduplicates = new HashSet<String>();
+	    		  System.out.println(txt);
+	    		  for(int checkanyotherword=1;checkanyotherword<=WORD_LENGTH;checkanyotherword++) {
+	    			 
+	    			  if(checkanyotherword!=innerdiv) {
+	    				  if(word.charAt(checkanyotherword-1)==word.charAt(innerdiv-1)) {
+	    					  WebElement checkduplicate=driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div/div["+outerdiv+"]/div["+checkanyotherword+"]/div"));
+	    					  System.out.println("checkduplicate"+word.charAt(checkanyotherword-1));
+	    					  String checkduplicatetxt=checkduplicate.getAttribute("outerHTML".valueOf("data-state")).split(" ").clone()[0];
+	    					  if(checkduplicatetxt=="correct") {
+	    						  checkduplicates.add("correct");
+	    						  break;
+	    					  }
+	    					  else if(checkduplicatetxt=="present"){
+	    						  checkduplicates.add("present");
+	    						  break;
+	    					  }
+	    					  else {
+	    						 
+	    					  }
+	    				  }
+	    				  
+	    			  }
+	    		  }
+	    		  if(checkduplicates.contains("correct")|| checkduplicates.contains("present")) {
+	    			  System.out.println("present at some other location");
+	    			  removeDictionarywords(wordSet,word.charAt(innerdiv-1),innerdiv-1,"present");
+	    		  }
+	    		  else {
+	    			  System.out.println("absent everywhere");
+	    			  removeDictionarywords(wordSet,word.charAt(innerdiv-1),innerdiv-1,"absent");
+	    		  }
 	    	  }
 	    
 	      }
 	      
 	      driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
 	     // System.out.println("------------------");
+	      if(outerdiv==1) {
+	    	  word="drive";
+	      }
 	      
 	      }
-
+	      
+	      for(String s:wordSet) {
+	    	  System.out.println(s);
+	      }
+	  
 
 	        System.out.println("Page title is: " + driver.getTitle());
 	        System.out.println("url" + driver.getCurrentUrl());
@@ -110,16 +152,19 @@ public class interact {
 		    	iterator.remove();
 		    	}
 		    }
-		    else if(method.equals("present") || method.equals("absent")){
+		    else if(method.equals("present")){
 		    	if(element.charAt(charposition)==chartocheck) {
 					iterator.remove();
 				    }	
 		    }
-		    
+		    else {
+		    	if(element.indexOf(chartocheck)!=-1) {
+		    		System.out.println("hereee");
+		    		iterator.remove();
+		    	}
+		    }
 		}
-		for(String s:wordSet) {
-			System.out.println(s);
-		}
+		
 		 System.out.println("wordset size after  ---- " +wordSet.size());
 		
 	}
