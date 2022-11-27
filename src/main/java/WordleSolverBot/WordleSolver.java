d30a6a1cf728796bd1ecc0da67eff535dd3a1019
pkondrakunta <pragnyakondrakunta@gmail.com>
2022-11-26 20:52:23 -0500

    created WordleSolver and WordleConnectorBot

---
+++ src/main/java/WordleSolverBot/WordleSolver.java
@@ -0,0 +1,209 @@
+package WordleSolverBot;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class WordleSolver {
+
+//	class variables
+	private static HashSet<String> wordSet = new HashSet<String>();;
+	private static Integer wordLength;
+
+	public static enum LetterHint {
+		NOT_PRESENT, PRESENT_AT_RIGHT_POSITION, PRESENT_AT_WRONG_POSITION
+	}
+
+	// Constructor
+	public WordleSolver(int length) throws IOException {
+
+		wordLength = length;
+		// Creating the Wordle words list
+		File file = new File("wordlist/valid-wordle-words.txt");
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String word;
+
+		while ((word = br.readLine()) != null) {
+			wordSet.add(word);
+		}
+
+	}
+
+	public String recommendWord(Hashtable<Integer, String[]> hints) throws IOException {
+		System.out.println("Recommending Words... " + hints.size());
+
+		String resultWord = new String();
+//		Set<Integer> keys = hints.keySet();
+
+//        Enumeration<Integer> e = hints.keys();
+
+		deduceHintsUpdateWords(hints);
+
+//        while (e.hasMoreElements()) {
+// 
+//            // Getting the key of a particular entry
+//            int key = e.nextElement();
+// 
+//			String letter = hints.get(key)[0];
+//			LetterHint letterInfo = LetterHint.valueOf(hints.get(key)[1]);
+//			System.out.println("Value of " + key + " is: [" + letter + ", " + letterInfo.name() + "]");
+//
+//			if (letterInfo.equals(LetterHint.PRESENT_AT_RIGHT_POSITION)
+//					|| letterInfo.equals(LetterHint.PRESENT_AT_WRONG_POSITION)) {
+//				updateWordSet(wordSet, letter.charAt(0), key, letterInfo);
+//			}
+//
+//			else if (letterInfo.equals(LetterHint.NOT_PRESENT)) {
+//				LetterHint checkDuplicateLetterInfo = checkDuplicates(hints, key, letter);
+//				
+//				if (checkDuplicateLetterInfo.equals(LetterHint.NOT_PRESENT)) {
+//					updateWordSet(wordSet, letter.charAt(0), key, LetterHint.NOT_PRESENT);
+//				}
+//				else if (checkDuplicateLetterInfo.equals(LetterHint.PRESENT_AT_RIGHT_POSITION)) {
+//					updateWordSet(wordSet, letter.charAt(0), key, LetterHint.PRESENT_AT_RIGHT_POSITION);
+//				}
+//				else if (checkDuplicateLetterInfo.equals(LetterHint.PRESENT_AT_WRONG_POSITION)) {
+//					updateWordSet(wordSet, letter.charAt(0), key, LetterHint.PRESENT_AT_WRONG_POSITION);
+//				}
+//				else {
+//					System.out.println("Entered else!! ");
+//				}
+//			}
+//			
+//        }
+
+		int wordsetindex = 1;
+		for (String s : wordSet) {
+			if (wordsetindex == 1) {
+				resultWord = s;
+				break;
+			}
+		}
+
+		if (wordSet.isEmpty()) {
+			System.out.println("I am out of words! What?!?!");
+		}
+		System.out.println(wordSet);
+		return resultWord;
+	}
+
+	private static void deduceHintsUpdateWords(Hashtable<Integer, String[]> hints) {
+
+		System.out.println("Deducing hints.. hints size " + hints.size());
+
+//        Enumeration<Integer> e = hints.keys();    
+//        while (e.hasMoreElements()) {
+//            // Getting the key of a particular entry
+//            int key = e.nextElement();
+//			String letter = hints.get(key)[0];
+//			LetterHint letterInfo = LetterHint.valueOf(hints.get(key)[1]);
+//    		System.out.println(key + ", " + letter);
+////			System.out.println("Value of " + key + " is: [" + letter + ", " + letterInfo.name() + "]");
+//
+//			if (letterInfo.equals(LetterHint.PRESENT_AT_RIGHT_POSITION)
+//					|| letterInfo.equals(LetterHint.PRESENT_AT_WRONG_POSITION)) {
+//				updateWordSet(wordSet, letter.charAt(0), key, letterInfo);
+//			}
+//
+//			else if (letterInfo.equals(LetterHint.NOT_PRESENT)) {
+//				LetterHint checkDuplicateLetterInfo = checkDuplicates(hints, key, letter);
+//				
+//				if (checkDuplicateLetterInfo.equals(LetterHint.NOT_PRESENT)) {
+//					updateWordSet(wordSet, letter.charAt(0), key, LetterHint.NOT_PRESENT);
+//				}
+//				else if (checkDuplicateLetterInfo.equals(LetterHint.PRESENT_AT_RIGHT_POSITION)) {
+//					updateWordSet(wordSet, letter.charAt(0), key, LetterHint.PRESENT_AT_RIGHT_POSITION);
+//				}
+//				else if (checkDuplicateLetterInfo.equals(LetterHint.PRESENT_AT_WRONG_POSITION)) {
+//					updateWordSet(wordSet, letter.charAt(0), key, LetterHint.PRESENT_AT_WRONG_POSITION);
+//				}
+//				else {
+//					System.out.println("Entered else!! ");
+//				}
+//			}
+//        }
+
+		Set<Integer> setOfKeys = hints.keySet();
+
+		// Iterating through the Hashtable
+		for (Integer i : setOfKeys) {
+			// Print and display the Rank and Name
+			System.out.println("Rank : " + i + "\t\t Name : " + hints.get(i)[0] + " " + hints.get(i)[1]);
+		}
+
+	}
+
+	private static LetterHint checkDuplicates(Hashtable<Integer, String[]> hints, Integer checkLetterIndex,
+			String letter) {
+		System.out.println("Checking duplicates for " + letter);
+
+		Set<Integer> duplicateKeys = hints.keySet();
+		for (Integer dKey : duplicateKeys) {
+
+			if (checkLetterIndex != dKey) {
+				String newLetter = hints.get(dKey)[0];
+				LetterHint newLetterInfo = LetterHint.valueOf(hints.get(dKey)[1]);
+
+				if (newLetter == letter && newLetterInfo.equals(LetterHint.PRESENT_AT_RIGHT_POSITION)) {
+					return LetterHint.PRESENT_AT_RIGHT_POSITION;
+				} else if (newLetter == letter && newLetterInfo.equals(LetterHint.PRESENT_AT_WRONG_POSITION)) {
+					return LetterHint.PRESENT_AT_WRONG_POSITION;
+				}
+			}
+		}
+		return LetterHint.NOT_PRESENT;
+
+	}
+
+	private static void updateWordSet(HashSet<String> wordSet, char chartocheck, int charposition, LetterHint method) {
+
+		System.out.println("Updating words with hint for " + chartocheck);
+
+//		System.out.println("Before, word set size: \t" + wordSet.size());
+		Iterator<String> iterator = wordSet.iterator();
+		while (iterator.hasNext()) {
+			String element = iterator.next();
+			if (method.equals(LetterHint.PRESENT_AT_RIGHT_POSITION)) {
+				if (element.charAt(charposition) != chartocheck || element.indexOf(chartocheck) == -1) {
+					iterator.remove();
+				}
+			} else if (method.equals(LetterHint.PRESENT_AT_WRONG_POSITION)) {
+				if (element.charAt(charposition) == chartocheck || element.indexOf(chartocheck) == -1) {
+					iterator.remove();
+				}
+			} else {
+				if (element.indexOf(chartocheck) != -1) {
+					iterator.remove();
+				}
+			}
+		}
+
+//		System.out.println("After, word set size: \t" + wordSet.size());
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		LetterHint hint1 = LetterHint.PRESENT_AT_RIGHT_POSITION;
+
+		Hashtable<Integer, String[]> hints_hashtable = new Hashtable<Integer, String[]>();
+		hints_hashtable.put(1, new String[] { "m", "PRESENT_AT_RIGHT_POSITION" });
+		hints_hashtable.put(2, new String[] { "o", "PRESENT_AT_RIGHT_POSITION" });
+		hints_hashtable.put(3, new String[] { "m", "NOT_PRESENT" });
+		hints_hashtable.put(4, new String[] { "m", "NOT_PRESENT" });
+		hints_hashtable.put(5, new String[] { "y", "NOT_PRESENT" });
+
+		WordleSolver wSolver = new WordleSolver(5);
+
+		String word = wSolver.recommendWord(hints_hashtable);
+	}
+
+}
--- src/main/java/WordleSolverBot/getRandomword.java
+++ src/main/java/WordleSolverBot/getRandomword.java
@@ -5,31 +5,30 @@ import java.util.Random;
 import java.util.Set;
 
 public class getRandomword {
-	public static <E> E getRandomElement(Set<? extends E> set)
-{
+	public static <E> E getRandomElement(Set<? extends E> set) {
 
-    Random random = new Random();
+		Random random = new Random();
 
-    int randomNumber = random.nextInt(set.size());
+		int randomNumber = random.nextInt(set.size());
 
-    Iterator<? extends E> iterator = set.iterator();
+		Iterator<? extends E> iterator = set.iterator();
 
-    int currentIndex = 0;
-    E randomElement = null;
+		int currentIndex = 0;
+		E randomElement = null;
 
-    while (iterator.hasNext()) {
+		while (iterator.hasNext()) {
 
-        randomElement = iterator.next();
+			randomElement = iterator.next();
 
-        // if current index is equal to random number
-        if (currentIndex == randomNumber)
-            return randomElement;
+			// if current index is equal to random number
+			if (currentIndex == randomNumber)
+				return randomElement;
 
-        // increase the current index
-        currentIndex++;
-    }
+			// increase the current index
+			currentIndex++;
+		}
 
-    return randomElement;
-}
+		return randomElement;
+	}
 
 }
