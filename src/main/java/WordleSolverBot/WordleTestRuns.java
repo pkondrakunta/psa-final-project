e4edd7620796ec2ba9435a0f77f517ce36fa5151
mahathisiddavatam <siddavatam.m@northeastern.edu>
2022-12-04 17:05:47 -0500

    Logging tests in csv

--- src/main/java/WordleSolverBot/WordleTestRuns.java
+++ src/main/java/WordleSolverBot/WordleTestRuns.java
@@ -1,15 +1,19 @@
 package WordleSolverBot;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 
 public class WordleTestRuns {
 
 	public static final int WORD_LENGTH = 5;
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		testInfo[] testData = new testInfo[50];
+		testInfo[] testData = new testInfo[100];
 
-		int testRuns = 50;
+		int testRuns = 100;
 
 		WordleSolver w = new WordleSolver(WORD_LENGTH);
 
@@ -29,8 +33,24 @@ public class WordleTestRuns {
 		for (int i = 0; i < testRuns; i++) {
 			System.out.println("Word: " + testData[i].getWord() + ", Attempts: " + testData[i].getAttempts());
 		}
-		
+
+		try {
+            FileOutputStream fis = new FileOutputStream("wordlist/testrun.csv");
+            OutputStreamWriter isr = new OutputStreamWriter(fis);
+            BufferedWriter bw = new BufferedWriter(isr);
+            int j = 50;
+            for (testInfo i : testData) {
+                String content = i.getWord() + "," + i.getAttempts()+ "\n";
+                bw.write(content);
+                bw.flush();
+            }
+            bw.close();
+            System.out.println("File created");
+		} catch (IOException e) {
+            e.printStackTrace();
+        }
 
 		// playWordle();
-	}
+	
+}
 }
